bb1c2e97299409640b3136df57926e578bf6dca4
V <goodros39@gmail.com>
2022-03-28 21:46:11 +0300

    opt 4

--- assets/php/foot.php
+++ assets/php/foot.php
@@ -3,7 +3,12 @@
 </div>
 </body>
 <? foreach ($styles as $style): ?>
-    <link rel="<?= $style['rel']; ?>" href="<?= $style['href']; ?>" media="all">
+    <link rel="<?= $style['rel']; ?>" href="<?= $style['href']; ?>" media="none" onload="if(media!='all')media='all'">
 <? endforeach; ?>
+<noscript>
+    <? foreach ($styles as $style): ?>
+        <link rel="<?= $style['rel']; ?>" href="<?= $style['href']; ?>">
+    <? endforeach; ?>
+</noscript>
 <script src="/js/app.js"></script>
 </html>
