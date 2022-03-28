e29d37181800ed50a9a455f4a870ac3f3fec3061
V <goodros39@gmail.com>
2022-03-28 21:38:45 +0300

    opt 2

--- assets/php/foot.php
+++ assets/php/foot.php
@@ -2,8 +2,8 @@
 <? include 'footer.php'; ?>
 </div>
 </body>
-<script src="/js/app.js"></script>
 <? foreach ($styles as $style): ?>
     <link rel="<?= $style['rel']; ?>" href="<?= $style['href']; ?>">
 <? endforeach; ?>
+<script src="/js/app.js"></script>
 </html>
