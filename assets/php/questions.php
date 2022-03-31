920b610b274e17ffb45cca99f693cd421a3dd878
V <goodros39@gmail.com>
2022-03-31 08:58:48 +0300

    content replaced

--- assets/php/questions.php
+++ assets/php/questions.php
@@ -1,27 +1,46 @@
 <?php
 return [
     [
-        'question' => 'Why do I need an online presence?',
-        'answer' => '',
+        'question' => 'How to order?',
+        'answer' => 'It is not difficult to get real followers using our service! To order, select a package, enter a link and specify the quantity. You will be redirected to the website, where you need to make a payment. After pay, your order will automatically will be fulfilled.<br><br>
+                    On our website, you can immediately see and take advantage of promotions and advantageous offers at a special price that are currently in effect.',
     ],[
-        'question' => 'How does Likes.io work?',
-        'answer' => 'Growing on Instagram involves getting your followers to engage in your content by viewing, liking, commenting, etc.
-                                    That means engaging with theirs as well, and that is where we come in. 
-                                    We engage with other users\' content on your behalf to get them to notice you through the notifications they receive or by appearing in their views. 
-                                    Your audience will also be encouraged to interact with your content once you have active engagement. 
-                                    We can help you create that impression with our convenient and cost-effective offers.<br>
-                                    The attention you get is through the notification that is sent to them when you like their content and by seeing your account in their views. 
-                                    If they like your content, they will come back to follow you and engage with your content. 
-                                    The only part you will have to play is to research what to post to get Instagram likes so that your visitor\'s chances of staying, or in this case, following you, are maximized. 
-                                    It is simple, safe, yet efficient and will yield the results you require.',
+        'question' => 'How long does it take for services to be performed?',
+        'answer' => 'The launch of likes and views, subscribers and story views is almost instant, in a matter of minutes after payment. The attraction of followers is done gradually, in order to protect your account. The timing may vary depending on the current state of the social network algorithms.<br><br>
+                    The main thing is that at the time of the order, your profile must be open (public). Promotion of closed (private) pages is not possible. Also, the account should not have age restrictions. Additionally, we do not recommend changing the login immediately after payment and before the service is rendered. 
+                    If this happens, we can lose your page data, and all the efforts spent on promotion burn out. In other words, you will simply lose all new followers.',
     ],[
-        'question' => 'Do I get support from Likes.io?',
-        'answer' => '',
+        'question' => 'What are the guarantees?',
+        'answer' => 'We conscientiously carry out all orders. When in doubt, start with a minimum order and a small number of followers – check our integrity.<br><br>
+                    Depending on the current algorithms of Instagram, promotion is done either only by live subscribers, or of a mixed type: some are real people, the rest are high-quality bots (robots) with avatars and publications. Therefore, if you want to know how to get Instagram followers – leave a request as soon as possible.<br><br>
+                    It should also be remembered that there are no risks, because modern Instagram follower boost ensures that you will never be excluded from this social network. As with every aspect of our service, we take your security seriously and in fact our Insta promotion offerings are a unique and innovative approach which means your acc will never be at risk of being banned. And all this at the best prices and with a minimum of your participation.',
     ],[
-        'question' => 'Why do you sell followers, likes, and views?',
-        'answer' => '',
+        'question' => 'Why <b>buy Instagram followers</b>?',
+        'answer' => '<p>Followers are a key indicator of popularity on Instagram, which contributes to the development of the profile. We offer secure online account promotion without registration and tasks.</p>
+                        <p>Increasing the quantity of followers on your account can allow you to:</p>
+                        <ul>
+                        <li>promote your brand (both business and personal);</li>
+                        <li>increase the credibility of the social media account ;</li>
+                        <li>raise the cost of selling ads on the page;</li>
+                        <li>increase conversion.</li>
+                        </ul>
+                        <p>And all you need is to order a suitable service on our website. We offer secure instant online account promotion.</p>',
     ],[
-        'question' => 'Does my account need to be public?',
-        'answer' => '',
+        'question' => 'What are your company\'s advantages?',
+        'answer' => '<p>A cheap and high-quality service for attracting visitors to a page on a social network is not just words, but reality. We will bring you subscribers and guarantee the result. If within a month there are fewer subscribers than you ordered, then we will return them at our own expense!</p>
+                    <p>Here are a few reasons to order subscribers from us:</p>
+                    <ul>
+                    <li>Convenient interface that allows you to order subscribers in two clicks. Using our service is convenient even for beginners.</li>
+                    <li>Extensive experience in the market, including promotion on various Internet resources and social networks.</li>
+                    <li>High quality – all accounts with avatars and publications.</li>
+                    <li>We provide the declared number of new users on your account.</li>
+                    <li>Guarantee against ban and premature leaving.</li>
+                    <li>Improvement of statistics, as along with new users who subscribe to you, impressions and coverage are added.</li>
+                    <li>Quick launch.</li>
+                    <li>Support of several payment methods: by card or using an electronic wallet, crypto, and other e-currencies.</li>
+                    <li>Cheap cost for such a service.</li>
+                    <li>24/7 support – we are always in touch to help you in solving any problems.</li>
+                    </ul>
+                    <p>You can place an order from any country and make payment in any way convenient for you.</p>',
     ],
 ];
--- public/home.php
+++ public/home.php
@@ -8,11 +8,12 @@ $description = '';
     <div class="wrap">
         <div class="row1">
             <h1 class="line1">
-                Buy Instagram followers and<br>
-                <b>grow your account faster</b>
+                <b>Buy Instagram Followers</b> and Create the Right Image for Your Brand
             </h1>
             <div class="line2">
-                Tired of spending loads of time trying to grow your Instagram? Buy Instagram followers safely and securely through Upleap. Fill in your username to get started:
+                What blogger doesn't secretly dream of having a million loyal fans? The race for visitors will continue as long as social networks exist.
+                Therefore, you need to think how to raise attendance while creating an account and <b>buy Instagram followers</b>.
+                Use the services of FollowerUs and increase your Instagram popularity in just a few clicks, simply and effectively!
             </div>
             <div class="line3">
                 <butt class="go_butt" href="/pricing">Simple Pricing</butt>
@@ -41,7 +42,7 @@ $styles['banner_sect'] = [
     <div class="wrap">
         <h2 class="row1">How It Works?</h2>
         <div class="row2">
-            Make your account stand out through a combination of automated likes and story views, and through buying followers.
+            Wondering how to get Instagram followers? It is very easy and cheap with our unique offers. Launch and promotion begins almost immediately after payment. When in doubt, start with the minimum order and see how it works.
         </div>
         <div class="row3">
             <div class="item">
@@ -49,9 +50,9 @@ $styles['banner_sect'] = [
                     <img class="icon" alt="" src="/img/how_work_sect/icon1.svg">
                 </div>
                 <div class="text_wrap">
-                    <div class="title">Grow organically</div>
+                    <div class="title">Select the required service package.</div>
                     <div class="desc">
-                        Real likes and story views will help you increase your reach and engagement.
+                        If you have any questions, our experts will advise you and help you choose the best offer.
                     </div>
                 </div>
             </div>
@@ -60,9 +61,9 @@ $styles['banner_sect'] = [
                     <img class="icon" alt="" src="/img/how_work_sect/icon2.svg">
                 </div>
                 <div class="text_wrap">
-                    <div class="title">Smart targeting</div>
+                    <div class="title">Specify your login and the number of followers.</div>
                     <div class="desc">
-                        Focus on your target audience based on their interests, location and hashtags.
+                        This data will help us to analyze your social network account and grow organically. We guarantee complete safety!
                     </div>
                 </div>
             </div>
@@ -71,9 +72,9 @@ $styles['banner_sect'] = [
                     <img class="icon" alt="" src="/img/how_work_sect/icon3.svg">
                 </div>
                 <div class="text_wrap">
-                    <div class="title">Security first</div>
+                    <div class="title">Make payment.</div>
                     <div class="desc">
-                        You'll never have to worry about your account being put at risk, we protect your reputation.
+                        You can pay for Instagram promotion in any convenient way: using a bank card, e-wallet, specialized online services (for instance, Apple Pay) and crypto.
                     </div>
                 </div>
             </div>
@@ -136,7 +137,7 @@ $styles['deals_sect'] = [
             Frequently Asked Questions
         </h2>
         <div class="row2">
-            Have a different question about how Likes.io works or the pricing plans available? Get in touch with one of our specialists.
+            Here you can find answers to the FAQ. If you need additional advice, please contact our specialists.
         </div>
         <div class="row3">
             <? foreach (include('../assets/php/questions.php') as $indx => $item): ?>
@@ -170,49 +171,54 @@ $styles['questions_sect'] = [
     <div class="wrap">
         <div class="row1">
             <div class="item">
-                <h2 class="title">How do you choose my likes?</h2>
+                <h2 class="title">Why Do You Need to <b>Buy Instagram Followers</b> and What Benefits Can It Bring?</h2>
                 <img class="image" alt="" src="/img/img_text_sect/img1.png">
                 <div class="desc">
-                    <p>
-                        We target accounts from people who are highly likely to want to see, like and engage with your own content.
-                        Our automated likes service takes your inputs for similar brands, competitor sites and key influencer profiles and sends likes to their posts.
-                        If you buy 50 Instagram likes, each one will be sent to a carefully selected account for maximum impact.
-                    </p>
-                    <p>
-                        With a service that uses location, hashtags and demographics to target your audience, you can be sure that every like counts.
-                        We maximize the possibility of return engagement by only liking posts that are deeply connected with your target market.
-                        It’s an efficient way to grow your brand on social media with no wasted effort.
-                    </p>
+                    <p>Every user of the social network met profiles, the number of subscribers of which goes off scale, and dreamed of the same glory. It turns out that for this, it is not necessary to be a famous blogger or a movie and TV star. Instagram's promotion can help in the realization of this dream.</p>
+                    <p>A subscriber is a person or account that is interested in viewing content, can create comment activity, and is potentially a customer. Accounts are divided into organic, which came because of their own desire and great interest in the product, and special, which came with the help of specialized services. But do not think that artificially attracted followers, just like organic ones, cannot be interested in the idea of ​​​​the page owner or the product offered. In the end, the right good or service will always find its consumer, but sometimes it needs a push.</p>
+                    <p>Most are wondering why <b>buy Instagram followers</b> if you can develop a page, and it's completely free. Let's talk in more detail:</p>
+                    <ul>
+                        <li>Instagram remains one of the most popular platforms in the world. This social network is installed in every 8th person in the world. Thanks to this, the site has an incredible number of people potentially interested in products or services. And only the largest corporations with a whole staff of SMM managers and marketers will be able to reach such an audience on their own.</li>
+                        <li>Convenient filters allow advertisers to fine-tune the advertising campaign, which increases the cost of each publication in the account, and, accordingly, the income of a blogger or just a popular person. And most importantly, our attracted users are real people who can not only be interested in your offer, but also recommend you to others.</li>
+                        <li>Companies with a promoted Instagram page inspire the trust of customers, especially young ones. For most girls and guys under 28, a good account gives more credibility than any other advertisement, including on TV.</li>
+                    </ul>
+                    <h3>Promotion for Ordinary Non-Commercial Accounts</h3>
+                    <p>Instant increasing the number of Instagram followers can change the life of any person. First of all, this applies to creative people who love their hobby and regularly share the results on social networks. Connoisseurs can easily see a promoted account, look at your work and appreciate it, or even buy it. This is a good way to find like-minded followers who can potentially become true friends.</p>
+                    <p>It’s really amazing, but attracting followers on Instagram can bring a lot of interesting people into a person’s life. Of course, communication with bots will not work out, but a cool account with several thousand fans will attract real followers much more than a small one. Thanks to this, a rock musician will be able to find a band, a waitress a fascinating job, and a dancer a new company of comrades.</p>
+                    <p>Conditioning the above, we can summarize: Instagram is a social platform in the development of which everyone is interested, from entrepreneurs to ordinary users. A paid increase in the number of subscribers from FollowerUs will help bring your cherished dream of becoming popular and famous closer. Attracted customers will buy products more often, and the company will get the opportunity to conduct a cheap but effective advertising campaign, notifying customers about new promotions and discounts on a platform convenient for them.</p>
                 </div>
             </div>
             <div class="item">
-                <h2 class="title">Why are Instagram likes valuable?</h2>
+                <h2 class="title">How to Get Instagram Followers?</h2>
                 <img class="image" alt="" src="/img/img_text_sect/img2.png">
                 <div class="desc">
-                    <p>
-                        Alongside comments, likes are the principal currency spent on Instagram.
-                        A like is an affirmation to the social media community that your post is useful, interesting or fun, and might be enjoyed by someone else too.
-                    </p>
-                    <p>
-                        Not only do likes indicate the quality of your post and brand in general, they also show that many people think your account is worth following.
-                        The more likes your account receives, the better the ‘social proof’ of your popularity and quality. More likes also mean more exposure.
-                        Instagram uses an algorithm to choose which posts to show to larger audiences and having a more popular post will encourage Instagram to organically boost your brand.
-                    </p>
+                    <p>Instagram is a trendy network for business and creativity, but before gaining recognition, you need to attract a lot of live followers and promote the account. Of course, you can do it yourself, but over time you will realize that you can no longer cope. And then, if you want to quickly add subscribers in large numbers, you need to look for paid services where you can do this.</p>
+                    <p>To <b>buy Instagram followers</b> you should make your account public and connect it to other social media profiles. Decide on a topic and target audience so important information will get to interested people. Then provide starter content and start searching for subscribers.</p>
+                    <p>Before using paid (incl. cheap) third-party services, you can try the free method, which is an exchange subscription. Choose topics that interest you and offer mutual subscription, this is quite effective. If this free technique is not enough, you can use special applications and services that allow you to post tasks. As a result, users will be added to your Insta after completing them.</p>
+                    <p>Another option is to use promotion services that sell real Instagram users who will become active participants in promoting your account. Constantly updating the main content of an acc with live followers will allow you to always stay afloat, without losing old and acquiring new subscribers.</p>
                 </div>
             </div>
             <div class="item">
-                <h2 class="title">Why is it a monthly service ideal?</h2>
+                <h2 class="title">Why Choose Our Paid Services?</h2>
                 <img class="image" alt="" src="/img/img_text_sect/img3.png">
                 <div class="desc">
-                    <p>
-                        One of the key problems that people and businesses face on social media is consistency.
-                        Initial interest in growth is often high but can become more challenging to maintain over time, especially if growth begins to slow.
-                    </p>
-                    <p>
-                        If your brand is inconsistent in the way you engage on social media, it will be hard to attract long-term followers who provide real value.
-                        If your efforts are sporadic, it will also be a challenge to build a reputation or sense of expertise in a market.
-                        By automating the monthly service, your followers will see your consistent approach and have faith that you are an account to be trusted.
-                    </p>
+                    <p>All methods of promotion differ primarily in the quality of work. Attracting subscribers can cost some money, which you spent on creating accounts, advertising & promoting your own page, and technical support. At the same time, all this helps to increase prestige and profitability. As for free programs and knowhow, they are often ineffective. You won’t be able to get free followers, but it’s easy to spend precious time.</p>
+                    <p>At best, suspicious sites will first force you to subscribe to hundreds of followers or put thousands of likes. This method is absolutely not suitable for self-respecting companies, because instant attraction of followers on Instagram will not work out that way, but people's news feed will be filled with various garbage. Will there be trust in an organization that is marked on the pages of strange girls and guys? Obviously not.</p>
+                    <p>Our company offers quality services at a low price. Buying Instagram followers has its fundamental advantages:</p>
+                    <ul>
+                        <li>The customer immediately sees why people come. The accounts are thoughtful, there are avatars, posts, and some activity that makes the social network algorithms worse in tracking down bots.</li>
+                        <li>Ability to get technical support at any time of the day or night. Contact our specialists and get help or an answer to any question that arises.</li>
+                        <li>More detailed customization. You can regulate the flow of followers and likes, create truthful activity and involve real people.</li>
+                        <li>Paid Insta extension will allow you to immediately see followers, understand the value of each person, and roughly estimate the pace of development in the future.</li>
+                    </ul>
+                    <p>The most important advantage that getting followers on Instagram for money can boast is the ability to save a huge amount of time on free actions. Usually, unreliable services ask you to fulfill some conditions in return. Our company has been honestly working for more than a year on the market and assures: having calculated the time spent on attracting even 100 subscribers, a blogger and any other account owner will immediately understand that it is much more profitable to buy them.</p>
+                    <p>This time can be spent much more efficiently, for example, by coming up with creative moves to promote your products or by directly creating beautiful photos, interesting stories, and quality content. Properly setting up automatic user acquisition is no match for mutual signing or mass following. Moreover, the platform’s algorithms have long learned to notice such provocations and separate them from ordinary users, thereby limiting the number of real followers.</p>
+                    <p>Both novice bloggers and celebrities with a multi-million audience try to buy followers on Instagram. Three years ago, the International Creative Management Partners (ICMP) agency conducted a study where they checked celebrity accounts for cheating. And even then it turned out that many of them use it with might and main. The Kardashian sisters, Ariana Grande, Katy Perry and other stars “got caught” on the distortion. Moreover, the percentage of “fake” individuals they have reached almost half. What can we say about modern stars.</p>
+                    <p>Buying subscribers is a much more frequent phenomenon than it seems. Many followers give the account more weight, which allows you to sell ads for more. That is why bloggers most often resort to paid services. Online stores also use promotion: real users are more likely to subscribe to a well-known profile.</p>
+                    <p>But if you decide to be wise and choose to buy Instagram followers, pay special attention to the company and the program that will be used for this. FollowerUs agency puts customer satisfaction above all else. This means that the best, professional service for each customer is provided here. You will get exactly what you want, and you can pay in any convenient way, both online using the generated link and crypto. Our service packages are thought out to the smallest detail, starting from order conditions and ending with high-quality support. A team of professionals worked on the creation, who did everything for your convenience. Contact us and we will definitely help you!</p>
+
+                    <p><a href="https://pastenow.ru/cfe5b7efaa073f41bfa7cea61804557b" target="_blank">https://pastenow.ru/cfe5b7efaa073f41bfa7cea61804557b</a></p>
+                    <p><a href="https://text.ru/antiplagiat/624371f78402a" target="_blank">https://text.ru/antiplagiat/624371f78402a</a></p>
                 </div>
             </div>
         </div>
