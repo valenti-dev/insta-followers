e4ddf5ed54fdfeb0282dd8b6322ff871ea269c18
V <goodros39@gmail.com>
2022-04-10 22:31:02 +0300

    version added

--- assets/php/foot.php
+++ assets/php/foot.php
@@ -2,13 +2,14 @@
 <? include 'footer.php'; ?>
 </div>
 </body>
+<? $version = '1.0.0'; ?>
 <? foreach ($styles as $style): ?>
-    <link rel="<?= $style['rel']; ?>" href="<?= $style['href']; ?>" media="none" onload="if(media!='all')media='all'">
+    <link rel="<?= $style['rel']; ?>" href="<?= $style['href']; ?>?v=<?= $version; ?>" media="none" onload="if(media!='all')media='all'">
 <? endforeach; ?>
 <noscript>
     <? foreach ($styles as $style): ?>
-        <link rel="<?= $style['rel']; ?>" href="<?= $style['href']; ?>">
+        <link rel="<?= $style['rel']; ?>" href="<?= $style['href']; ?>?v=<?= $version; ?>">
     <? endforeach; ?>
 </noscript>
-<script src="/js/app.js"></script>
+<script src="/js/app.js?v=<?= $version; ?>"></script>
 </html>
