1fc5ee7b782443484916b7b428ba06220b58e909
V <goodros39@gmail.com>
2022-04-23 13:55:09 +0300

    emails upd

--- public/emails/welcome.php
+++ public/emails/welcome.php
@@ -11,7 +11,7 @@
 </head>
 <body style="margin: 0; padding: 0; background-color: #4A4A4A;">
 <div style="background-color: #ffffff; padding: 20px 40px 53px; border-radius: 10px; margin: 40px auto; width: 457px; max-width: 100%; box-sizing: border-box;">
-    <img alt="" src="//<?= $_SERVER['SERVER_NAME']; ?>/img/emails/hello.png" style="width: 278px; max-width: 100%; display: block; margin: 0 auto 44px;">
+    <img alt="" src="https://<?= $_SERVER['SERVER_NAME']; ?>/img/emails/hello.png" style="width: 278px; max-width: 100%; display: block; margin: 0 auto 44px;">
     <h1 style="font-weight: 900; font-size: 32px; line-height: 1.235; color: #292E43; text-align: center; margin: 0 auto 16px; font-family: 'Zen Kaku Gothic Antique', sans-serif;">Welcome in Followerus</h1>
     <div style="line-height: 1.575; color: #292E43; text-align: center; font-size: 18px; font-weight: 400; font-family: 'Zen Kaku Gothic Antique', sans-serif; margin: 0 auto 33px;">
         Thank you for the purchase<br>
@@ -19,7 +19,7 @@
     </div>
     <a href="#" style="display: flex; max-width: 100%; width: 241px; box-sizing: border-box; align-items: center; justify-content: space-between; padding: 0 16px 0 32px; background-color: #81B3FF; cursor: pointer; color: #ffffff; height: 60px; border-radius: 30px; text-decoration: none; line-height: 1.465; margin: 0 auto;">
         <span style="font-family: 'Zen Kaku Gothic Antique', sans-serif; font-size: 16px; font-weight: 500; margin: auto auto auto 0">Go to shopping</span>
-        <img alt="" src="//<?= $_SERVER['SERVER_NAME']; ?>/img/emails/butt-arrow.png" style="height: 26px">
+        <img alt="" src="https://<?= $_SERVER['SERVER_NAME']; ?>/img/emails/butt-arrow.png" style="height: 26px">
     </a>
 </div>
 </body>
