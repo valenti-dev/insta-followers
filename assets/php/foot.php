7dcdcf8bdf84d128a6d8bf69a07a0eafc1a26657
V <goodros39@gmail.com>
2022-05-07 10:41:20 +0300

    order popup upd

--- assets/php/foot.php
+++ assets/php/foot.php
@@ -2,7 +2,7 @@
 <? include 'footer.php'; ?>
 </div>
 </body>
-<? $version = '1.0.0'; ?>
+<? $version = '1.0.1'; ?>
 <? foreach ($styles as $style): ?>
     <link rel="<?= $style['rel']; ?>" href="<?= $style['href']; ?>?v=<?= $version; ?>" media="none" onload="if(media!='all')media='all'">
 <? endforeach; ?>
