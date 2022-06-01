24f9276a5835b379b998dd3eb01e752281ef173a
V <goodros39@gmail.com>
2022-06-01 10:30:33 +0300

    заменить текст в модалке

--- assets/php/foot.php
+++ assets/php/foot.php
@@ -2,7 +2,7 @@
 <? include 'footer.php'; ?>
 </div>
 </body>
-<? $version = '1.0.14'; ?>
+<? $version = '1.0.15'; ?>
 <? foreach ($styles as $style): ?>
     <link rel="<?= $style['rel']; ?>" href="<?= $style['href']; ?>?v=<?= $version; ?>" media="none" onload="if(media!='all')media='all'">
 <? endforeach; ?>
